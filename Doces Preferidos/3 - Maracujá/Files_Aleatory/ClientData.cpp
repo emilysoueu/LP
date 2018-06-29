d058cc90ee3380f6c5f8c812d411f19656a41e33
Emily Costa <emilys2costa@gmail.com>
2018-06-29 15:56:27 -0300

    Update ClientData.cpp

--- Doces Preferidos/3 - Maracujá/Files_Aleatory/ClientData.cpp
+++ Doces Preferidos/3 - Maracujá/Files_Aleatory/ClientData.cpp
@@ -22,20 +22,12 @@ int ClientData :: getAccountNumber () const{
 void ClientData :: setAccountNumber(int AccountNumberValue){
 	AccountNumber = AccountNumberValue;
 }
-/*==================================================*/
-// Aqui, a função tava retornando Inteiro, mas a declração lá no ClientData.h Tá
-// Dizendo que ele retorna String.
-
-// Versão antiga
-// int ClientData :: getLastName () const{
-// 	return LastName;
-// }
 
 // obtem o valor do LastName
 string ClientData :: getLastName () const{
 	return LastName;
 }
-/*==================================================*/
+
 
 //configurar o valor do LastName
 void ClientData :: setLastName(string LastNameString){
@@ -43,10 +35,9 @@ void ClientData :: setLastName(string LastNameString){
 	const char* LastNameValue = LastNameString.data();
 	int length = LastNameString.size();
 
-  // 1x MDS NÃO USA OPERADOR TERNÁRIO EM NOME DE G-ZUIS Hueahueuhuea
-	//length = (length < 15 ? length :: 14); //O erro é que era só ":" e não "::"
+  "
 
-  //If é coisa de gente, vlw flw.
+  
   if (length < 15){
     length = length;
   }else{
@@ -57,22 +48,13 @@ void ClientData :: setLastName(string LastNameString){
 	LastName[length] = '\0';// acrescenta caractere nulo ao sobrenome
 }
 
-/*==================================================*/
-// Aqui tu botou o "s" sem querer depois do "t" e invés de antes. (FirstName)
-//E também a função tava retornando Inteiro, mas a declração lá no ClientData.h Tá
-// Dizendo que ele retorna String.
-
-
-// Versão antiga
-// int ClientData :: getFirtsName () const{
-// 	return FirstName;
-// }
+/
 
 // obtem o valor do FirstName
 string ClientData :: getFirstName () const{
 	return FirstName;
 }
-/*==================================================*/
+
 
 //configurar o valor do FirstName
 void ClientData :: setFirstName(string FirstNameString){
@@ -80,17 +62,7 @@ void ClientData :: setFirstName(string FirstNameString){
 	const char*FirstNameValue = FirstNameString.data();
 	int length =FirstNameString.size();
 
-  // 2x MDS NÃO USA OPERADOR TERNÁRIO EM NOME DE G-ZUIS Hueahueuhuea
-  // Mesma mutreta do outro de cima. era ":" invés de "::"
-  // mas como você é uma menina amorzinho, EU TENHO CERTEZA que não vai mais usar
-  // operador ternário. u.u
-
-  //length = (length < 10 ? length :: 9);//como tava
-  //length = (length < 10 ? length : 9); // o jeito """"""""""certo""""""""""
-
-  // Agora tirando a zoeira. O if é mais legível. Antigamente todos os livros
-  // usavam operador ternário, era uma mania mesmo. Mas já caiu em desuso.
-  // Aí toda vez que tu ver operador ternário, tu só troca por um if bonitinho.
+  
   if (length < 10){
     length = length;
   }else{
@@ -103,20 +75,13 @@ void ClientData :: setFirstName(string FirstNameString){
 
 //obtem o valor do saldo
 double ClientData :: getBalance () const{
-  // Aqui tá retornando Balance com "B" maiusculo
-  // Lá no ClientData.h tá escrito com "b" minusculo
-  // Versão antiga:
-	// return Balance;
-
+  
 	return balance;
 }
 
 //configura o valor do saldo
 
 void ClientData :: setBalance(double BalanceValue){
-  // Aqui tá "setando" Balance com "B" maiusculo
-  // Lá no ClientData.h tá escrito com "b" minusculo
-  // Versão antiga:
-	// Balance = BalanceValue;
+  /
 	balance = BalanceValue;
 }
